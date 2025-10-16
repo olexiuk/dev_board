40fcc24ffa0f3c1086d9708a8cae86ed34f5a031
Connor Olexiuk <olexiukc@gmail.com>
2025-10-16 13:57:05 -0600

    Make LED blink

--- app/src/main.c
+++ app/src/main.c
@@ -3,7 +3,7 @@
 
 #define LED0_NODE DT_ALIAS(led0)
 
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios)
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
@@ -12,13 +12,14 @@ int main(void)
     if (!gpio_is_ready_dt(&led0))
         return -1;
 
-    ret = gpio_pin_confiure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
     if (ret < 0)
         return ret;
 
     while (1)
     {
-        
+        gpio_pin_toggle_dt(&led0);
+        k_msleep(1000);
     }
     return 0;
 }

01c8b45943e4837a504c23725124a5996520e0f8
Connor Olexiuk <olexiukc@gmail.com>
2025-10-16 20:00:21 -0600

    Button input with bounce problem

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,38 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
+#define SW0_NODE DT_ALIAS(sw0)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+static struct gpio_callback button_isr_data;
+
+void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    printk("Button 0 pressed\n");
+}
+
 int main(void) {
+    int ret;
 
-  while(1) {
+    if (!gpio_is_ready_dt(&button))
+        return 0;
 
-  }
-	return 0;
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0)
+        return ret;
+
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret < 0)
+        return ret;
+
+    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
+    gpio_add_callback(button.port, &button_isr_data);
+    
+    while(1)
+    {
+
+    }
+
+    return 0;
 }

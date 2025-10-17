0757692ae47b74503f8a60ad97e99ecfee93ec51
Connor Olexiuk <olexiukc@gmail.com>
2025-10-16 20:31:22 -0600

    4 bit binary counter

--- app/src/main.c
+++ app/src/main.c
@@ -18,13 +18,26 @@ int main(void) {
     ret = LED_init();
     if (ret < 0)
         return ret;
+
+    const enum led_id_t leds[] = {LED0, LED1, LED2, LED3};
     
+    unsigned char val = 0; 
+
     while(1)
     {
         if (BTN_check_clear_pressed(BTN0))
         {
-            LED_toggle(LED0);
-            printk("Button 0 pressed\n");
+            if (val < 0b1111)
+                val++;
+            else
+                val = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if ((val >> i) & 1)
+                    LED_set(leds[i], LED_ON);
+                else
+                    LED_set(leds[i], LED_OFF);
+            }
         }
         k_msleep(SLEEP_MS);
     }

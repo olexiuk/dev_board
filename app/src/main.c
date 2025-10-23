4a99d9d6a916ac77af4493645e5a8cd444021836
Connor Olexiuk <olexiukc@gmail.com>
2025-10-22 23:41:52 -0600

    Password checker

--- app/src/main.c
+++ app/src/main.c
@@ -6,7 +6,7 @@
 #include "BTN.h"
 #include "LED.h"
 
-#define SLEEP_MS 1
+#define SLEEP_MS 10
 
 int main(void) {
     int ret;
@@ -20,26 +20,62 @@ int main(void) {
         return ret;
 
     const enum led_id_t leds[] = {LED0, LED1, LED2, LED3};
+    const enum btn_id_t btns[] = {BTN0, BTN1, BTN2, BTN3};
     
-    unsigned char val = 0; 
+    // each two bits represents a button
+    // password can be different lengths
+    
+    // password is 1233
+    // uint16_t password = 0b01101111;
+    // uint8_t passwordLength = 4;
 
+    // password is 12332133
+    uint16_t password = 0b0110111110011111;
+    uint8_t passwordLength = 8;
+
+    uint16_t input = 0;
+    bool waiting = false;
+    int8_t buttonPressed;
+
+    // used to clear any bits past the length of the password
+    uint16_t passwordCap = 0b1111111111111111 >> (16 - (passwordLength * 2));
+
+    LED_set(leds[0], LED_ON);
+    
     while(1)
     {
-        if (BTN_check_clear_pressed(BTN0))
-        {
-            if (val < 0b1111)
-                val++;
-            else
-                val = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                if ((val >> i) & 1)
-                    LED_set(leds[i], LED_ON);
-                else
-                    LED_set(leds[i], LED_OFF);
-            }
-        }
         k_msleep(SLEEP_MS);
+        buttonPressed = -1;
+        for (int i = 0; i < 4; i++)
+        {
+            if (BTN_check_clear_pressed(btns[i]))
+                buttonPressed = i;
+        }
+        if (buttonPressed == -1)
+            continue;
+        if (waiting)
+        {
+            LED_set(leds[0], LED_ON);
+            waiting = false;
+            continue;
+        }
+        if (buttonPressed == 3)
+        {
+            if (password == input)
+            {
+                printk("Correct!\n");
+                LED_set(leds[0], LED_OFF);
+                waiting = true;
+            }
+            else
+            {
+                printk("Incorrect!\n");
+            }
+            input = 0;
+            continue;
+        }
+        // add button pressed to front of input and clear bits that are passed the length of the password
+        input = (input << 2 | (buttonPressed + 1)) & passwordCap;
     }
     return 0;
 }
